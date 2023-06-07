73ecb6b5c9c5c2fce7f3a37ab88a05c84f1bbecd
pangxiaobo <pangxiaobo@vchangyi.com>
2023-06-07 18:37:48 +0800

    update proxy domain

--- frontend/src/stores/modules/chat/index.ts
+++ frontend/src/stores/modules/chat/index.ts
@@ -28,7 +28,7 @@ export const useChatStore = defineStore(
         label: 'Bing 官方',
       },
       {
-        baseUrl: 'https://sydney.vcanbb.chat',
+        baseUrl: 'https://chat.xtech.ink',
         label: 'Cloudflare',
       },
       {
